59b24f8888cdf8c1afe1c14a86abb5dfa7e1930e
b02901157 <b02901157@ntu.edu.tw>
2017-05-15 22:55:45 +0800

    temp done

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -8,26 +8,65 @@ import './css/ionicons.css';
 class TodoItem extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+      changeName: this.props.itemName.substr(this.props.itemName.indexOf(' ') + 1),
+      changeNameHolder: 'Type to change',
+      editing: 0,
+    };
+    this.clickEditItemName = this.clickEditItemName.bind(this);
+  }
+  clickEditItemName() {
+    this.setState({ editing: 1 });
   }
   render() {
     return ((this.props.showMode - this.props.itemState) === 0 ?
       null :
-      <div className="todoItem">
-        {this.props.itemState === 0 ?
-          <i
-            className="icon ion-android-checkbox-outline-blank checkBox"
-            onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
-          /> :
-          <i
-            className="icon ion-android-checkbox-outline checkBox"
-            onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
-          />
+      <div>
+        {this.state.editing === 0 ?
+          <div className="todoItem">
+            {this.props.itemState === 0 ?
+              <i
+                className="icon ion-android-checkbox-outline-blank checkBox"
+                onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+              /> :
+              <i
+                className="icon ion-android-checkbox-outline checkBox"
+                onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+              />
+            }
+            <div className="itemName">{this.props.itemName.substr(this.props.itemName.indexOf(' ') + 1)}</div>
+            <i
+              className="icon ion-edit myIcon sideBarEdit"
+              onClick={this.clickEditItemName}
+            ></i>
+            <i
+              className="icon ion-trash-b myIcon"
+              onClick={() => this.props.deleteItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+            ></i>
+          </div> :
+          <div className="todoItem">
+            {this.props.itemState === 0 ?
+              <i
+                className="icon ion-android-checkbox-outline-blank checkBox"
+                onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+              /> :
+              <i
+                className="icon ion-android-checkbox-outline checkBox"
+                onClick={() => this.props.checkItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+              />
+            }
+            <div className="itemName">{this.props.itemName.substr(this.props.itemName.indexOf(' ') + 1)}</div>
+            <i
+              className="icon ion-log-in myIcon sideBarEdit"
+              onClick={this.submitFunction}
+            ></i>
+            <i
+              className="icon ion-trash-b myIcon"
+              onClick={() => this.props.deleteItemsFunc(Number(this.props.itemName.split(' ', 1)))}
+            ></i>
+          </div> 
         }
-        <div className="itemName">{this.props.itemName.substr(this.props.itemName.indexOf(' ') + 1)}</div>
-        <i
-          className="icon ion-trash-b myIcon"
-          onClick={() => this.props.deleteItemsFunc(Number(this.props.itemName.split(' ', 1)))}
-        ></i>
       </div>
     );
   }
